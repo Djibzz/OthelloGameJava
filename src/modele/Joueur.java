a5fa3c4404744642f5d6acc90ec84a82c00ae26f
Rayaleh Keynane <keynane.rayaleh@etu.univ-orleans.fr>
2025-03-06 13:55:51 +0100

    changement dans joueue

--- src/modele/Joueur.java
+++ src/modele/Joueur.java
@@ -1,5 +1,26 @@
 package modele;
 
 public class Joueur {
+    // Les Attributs
+    private String Pseudo;
+    private  int NbPartiesgagnés;
+
+    //Les Accesseurs
+
+    public String getPseudo() {
+        return Pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        Pseudo = pseudo;
+    }
+
+    public int getNbPartiesgagnés() {
+        return NbPartiesgagnés;
+    }
+
+    public void setNbPartiesgagnés(int nbPartiesgagnés) {
+        NbPartiesgagnés = nbPartiesgagnés;
+    }
 
 }
